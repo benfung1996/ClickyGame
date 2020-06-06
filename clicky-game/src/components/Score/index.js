fee60ba21ca9c568968af613aacbce8cb23a2b5d
Ben Fung <benfung1996@gmail.com>
2020-06-06 11:25:41 -0700

    Update index.js

--- clicky-game/src/components/Score/index.js
+++ clicky-game/src/components/Score/index.js
@@ -3,7 +3,8 @@ import "./style.css";
 
 function Score(props) {
   return <h1 className="gameScore">
-    <h3 className="score">Score: {props.status}</h3>
+    <h3 className="status">Status: {props.status}</h3>
+    <h3 className="score">Score: {props.score}</h3>
     <h3 className="goal">Goal: {props.goal}</h3>
     </h1>
 };
