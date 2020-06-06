43f8db12c60ac5ea6efad2066d8d2c92f11b7128
Ben Fung <benfung1996@gmail.com>
2020-06-06 16:25:36 -0700

    update

---
+++ build/precache-manifest.9966ebc7f156d75f08bf41c2e1625f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce83146a1b2b0fc806fc5df7bc2b44b",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "49bff5d13eb5a972f2b6",
+    "url": "/ClickyGame/static/css/main.b97e2acc.chunk.css"
+  },
+  {
+    "revision": "55ea0a1c41e3cf21f12c",
+    "url": "/ClickyGame/static/js/2.378b37ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.378b37ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bff5d13eb5a972f2b6",
+    "url": "/ClickyGame/static/js/main.37efa2ea.chunk.js"
+  },
+  {
+    "revision": "80adafc84e25d4e5b84c",
+    "url": "/ClickyGame/static/js/runtime-main.a190d07a.js"
+  }
+]);
